6df4892fae46fd773cf5f7d721d47a93fde82b91
prafessor <olegmilyo@gmail.com>
2024-11-11 13:19:56 +0200

    Do task of JS

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function checkForSpam(message) {
+  return (
+    (message.toLowerCase().indexOf('spam') === -1 ? false : true) ||
+    (message.toLowerCase().indexOf('sale') === -1 ? false : true)
+  );
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
